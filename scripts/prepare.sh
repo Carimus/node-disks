0e23280f4a793851ab8b2b1e8253cbc47752fc05
Brandon Phillips <bericp1@gmail.com>
2019-04-05 10:57:47 -0600

    chore(ci): one more small tweak to make prepare.sh more resilient

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # Update the package.json to place the build command in prepublishOnly instead of in postinstall
 json -I -f package.json -e 'this.scripts.prepublishOnly = "yarn run build"; delete this.scripts.postinstall;'
 
