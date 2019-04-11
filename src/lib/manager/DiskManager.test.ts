69c5aaf004f07e0c92ac58015d50521eca78304a
Brandon Phillips <bericp1@gmail.com>
2019-04-11 10:01:09 -0600

    refactor(*): split driver from disk config

--- src/lib/manager/DiskManager.test.ts
+++ src/lib/manager/DiskManager.test.ts
@@ -1,11 +1,12 @@
 import { DiskManager } from './DiskManager';
-import { DiskDriver } from './types';
+import { DiskDriver, LocalDisk } from '../..';
+import * as path from 'path';
 
 test('getDisk returns the same disk instance for the same disk name', () => {
     const diskMan = new DiskManager({
-        default: { driver: DiskDriver.Memory },
+        default: 'foo',
         foo: { driver: DiskDriver.Memory },
-        bar: 'foo',
+        bar: { driver: DiskDriver.Memory },
     });
     const defaultDisk1 = diskMan.getDisk();
     const defaultDisk2 = diskMan.getDisk();
@@ -15,20 +16,31 @@ test('getDisk returns the same disk instance for the same disk name', () => {
 
     expect(defaultDisk1).toBe(defaultDisk2);
     expect(fooDisk1).toBe(fooDisk2);
-    expect(defaultDisk1).not.toBe(fooDisk1);
-    expect(barDisk).toBe(fooDisk1);
+    expect(defaultDisk1).toBe(fooDisk1);
+    expect(barDisk).not.toBe(fooDisk1);
 });
 
 test('getDisk uses resolved name and not alias for the name of resolved disks', () => {
     const diskMan = new DiskManager({
-        default: { driver: DiskDriver.Memory },
+        default: 'foo',
         foo: { driver: DiskDriver.Memory },
         bar: 'foo',
     });
     const defaultDisk = diskMan.getDisk();
     const fooDisk = diskMan.getDisk('foo');
     const barDisk = diskMan.getDisk('bar');
-    expect(defaultDisk.getName()).toBe('default');
+    expect(defaultDisk.getName()).toBe('foo');
     expect(fooDisk.getName()).toBe('foo');
     expect(barDisk.getName()).toBe('foo');
+});
+
+test('DiskManager is backwards compatible with the old config style', () => {
+    const diskMan = new DiskManager({
+        default: 'foo',
+        // @ts-ignore since we're explicitly using an old unsupported type.
+        foo: { driver: DiskDriver.Local, root: '/tmp' },
+    });
+
+    const disk = diskMan.getDisk() as LocalDisk;
+    expect(disk.getRootPath()).toBe(path.resolve('/tmp'));
 });
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,42 +1,11 @@
-import { S3 } from 'aws-sdk';
-
-/**
- * Available drivers
- */
-export enum DiskDriver {
-    Memory = 'memory',
-    Local = 'local',
-    S3 = 's3',
-}
-
 /**
  * The common config options for all disks.
  */
 export interface DiskConfig {
     /**
-     * The driver to use.
+     * Other config options are possible depending on the driver.
      */
-    driver: DiskDriver;
-}
-
-/**
- * A map/dictionary of disk names to their config objects. If the value is a
- * string, than it's considered an alias and looked up recursively from the
- * root of the config.
- */
-export interface DiskManagerConfig {
-    default: string | DiskConfig;
-    [key: string]: string | DiskConfig;
-}
-
-/**
- * Options that can be passed to the DiskManager
- */
-export interface DiskManagerOptions {
-    /**
-     * A pre-initialized s3 client to use instead of creating a new one.
-     */
-    s3Client?: S3;
+    [key: string]: any;
 }
 
 /**
